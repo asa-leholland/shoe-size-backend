7128ac02656ed0a70f6687014bd8ce5270a0cf15
asa-leholland <asaleholland@gmail.com>
2023-04-09 18:29:35 -0700

    feat: Update to calculate size

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import json
 
-def convert_size(data_path, gender=None, size=None, location=None, converted_location=None):
+from enum import Enum
+
+
+class ShoeSizeCategory(Enum):
+    WOMENS = "Women's"
+    MENS = "Men's"
+    BIG_KIDS = "Big Kids' (7–12 Years)"
+    LITTLE_KIDS = "Little Kids' (4–7 Years)"
+    TODDLERS = "Toddlers' (9 Months - 4 Years)"
+    INFANTS = "Infants' (0–9 Months)"
+
+
+class ShoeSizeSystem(Enum):
+    US_CANADA = "US & Canada"
+    UK = "UK"
+    EUROPE = "Europe"
+    INCHES = "Inches"
+    CENTIMETERS = "Centimeters"
+
+
+def convert_size(data_path: str, unit: ShoeSizeCategory, size: str, location: ShoeSizeSystem, converted_location: ShoeSizeSystem):
+    assert unit in ShoeSizeCategory
+    assert location in ShoeSizeSystem
+    assert converted_location in ShoeSizeSystem
+    assert data_path is not None and data_path != "" and data_path.endswith(".json")
+    assert size is not None and size != "" and isinstance(size, str)
     with open(data_path) as f:
         data = json.load(f)
 
-    if not any([gender,size,location,converted_location]):
-        return data
-
-    if not any([location,converted_location,size]):
-        # filter the result so it is only matching the gender
-        gender_filtered = []
-        for entry in data:
-            if entry["Unit"] == gender:
-                gender_filtered.append(entry)
-        return gender_filtered
-
     for item in data:
-        if item["Unit"] == gender:
-            if not any([size]):
-                return item
-            index = item[location].index(size)
-            return item[converted_location][index]
+        if item["Unit"] == unit.value:
+            print(item[location.value])
+            index = item[location.value].index(size)
+            return item[converted_location.value][index]
